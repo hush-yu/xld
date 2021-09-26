18c578037c82aef2d423f3c9d996808922d8dac6
hush-yu <83074236+hush-yu@users.noreply.github.com>
2021-09-26 20:51:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const keys = document.querySelectorAll(".piano button");
+
+keys.forEach(item => {
+    item.addEventListener('click', () => playPiano(item.dataset.key))
+})
+
+const audioElement = document.querySelector("#audioElement")
+
+const playPiano = key => {
+    const url = `./audio/${key}.mp3`
+    audioElement.src = url;
+    audioElement.play()
+}
